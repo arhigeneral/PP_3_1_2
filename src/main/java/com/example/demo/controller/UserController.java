97ecd3fd314eacd3a5a97592cd397fc128052ada
sad_c <sad_casd@mail.ru>
2024-05-02 21:30:14 +0300

    Mapping update

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
 
@@ -23,38 +25,38 @@ public class UserController {
     }
 
 
-    @GetMapping("/users")
+    @GetMapping("")
     public String findAll(ModelMap model){
         List<User> users = userService.findAll();
         model.addAttribute("users", users);
         return "user-list";
     }
 
-    @GetMapping("/user-create")
+    @GetMapping("/create")
     public String createUserForm(User user){
         return "user-create";
     }
 
-    @PostMapping("/user-create")
+    @PostMapping("/create")
     public String createUser(User user){
         userService.save(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/user-delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") Long id){
         userService.deleteById(id);
         return "redirect:/users";
     }
 
-    @GetMapping("/user-update/{id}")
+    @GetMapping("/update/{id}")
     public String updateUserForm(@PathVariable("id") Long id, ModelMap model){
         User user = userService.findById(id);
         model.addAttribute("user", user);
         return "user-update";
     }
 
-    @PostMapping("/user-update")
+    @PostMapping("/update")
     public String updateUser(User user){
         userService.save(user);
         return "redirect:/users";
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -10,7 +10,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "first_name")
+    @Column(name = "name")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
